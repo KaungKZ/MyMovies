dc9fd19d5152ec6f4b5df6225f31c95991c8422c
Kaung Sithu <kgcthu.kev@gmail.com>
2022-01-28 04:59:54 +0630

    download functionality for various qualities

--- components/DownloadPopup.jsx
+++ components/DownloadPopup.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react'
 
-export default function DownloadPopup({ isOpen }) {
+export default function DownloadPopup({ isOpen, result }) {
   // console.log(isOpen);
 
   // useEffect(() => {
@@ -11,9 +11,18 @@ export default function DownloadPopup({ isOpen }) {
   return (
     <>
       {/* <div className="download-overlay w-full h-full fixed bg-gray-800 top-0 left-0 opacity-50 z-100"></div> */}
-      <div className="download fixed w-80 bg-gray-100 p-10 h-40 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <div className="download__container"></div>
+      <div className='download fixed w-80 bg-gray-100 p-10 h-40 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+        <div className='download__container flex flex-row align-center justify-center'>
+          {result.map((detail, index) => (
+            <button
+              key={index}
+              className='py-3 px-6 bg-green-400 rounded text-white hover:bg-green-500 transition flex items-center text-base mx-2'
+            >
+              <a href={detail.url}>{detail.quality}</a>
+            </button>
+          ))}
+        </div>
       </div>
     </>
-  );
+  )
 }
--- components/HomeMovieCategories.jsx
+++ components/HomeMovieCategories.jsx
@@ -1,33 +1,34 @@
-import React, { useState, useEffect } from "react";
-import Link from "next/link";
+/* eslint-disable jsx-a11y/alt-text */
+import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
 // import CategoryTitleBgShape from "/static/assets/section-title-bg-shape.png";
-import Image from "next/image";
-import CategoryBgShape from "../public/static/assets/section-content-bg-shape.svg";
-import Notfound from "../public/static/assets/error.svg";
+import Image from 'next/image'
+import CategoryBgShape from '../public/static/assets/section-content-bg-shape.svg'
+import Notfound from '../public/static/assets/error.svg'
 
-import { BlurhashCanvas } from "react-blurhash";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper.min.css";
-import "swiper/components/pagination/pagination.min.css";
-import "swiper/components/navigation/navigation.min.css";
-import { useRouter } from "next/router";
+import { BlurhashCanvas } from 'react-blurhash'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper.min.css'
+import 'swiper/components/pagination/pagination.min.css'
+import 'swiper/components/navigation/navigation.min.css'
+import { useRouter } from 'next/router'
 
 import {
   ChevronRightIcon,
   ChevronLeftIcon,
   ArrowNarrowRightIcon,
-} from "@heroicons/react/outline";
+} from '@heroicons/react/outline'
 
-import SwiperCore, { Pagination, Navigation } from "swiper/core";
+import SwiperCore, { Pagination, Navigation } from 'swiper/core'
 
-SwiperCore.use([Pagination, Navigation]);
+SwiperCore.use([Pagination, Navigation])
 
 // import {useState, useEffect} from
 
 export default function HomeMovieCategories(props) {
-  const [movieData, setMovieData] = useState({ data: [] });
-  const [swiper, setSwiper] = useState(null);
-  const router = useRouter();
+  const [movieData, setMovieData] = useState({ data: [] })
+  const [swiper, setSwiper] = useState(null)
+  const router = useRouter()
 
   // console.log(movieData);
 
@@ -39,20 +40,20 @@ export default function HomeMovieCategories(props) {
   // const router = useRouter();
 
   useEffect(() => {
-    setMovieData({ ...props });
+    setMovieData({ ...props })
 
     // if (swiper) {
     //   swiper.slideToLoop(0, 0, false);
     // }
-  }, [props]);
+  }, [props])
 
   // console.log(props);
 
   useEffect(() => {
     if (swiper?.params && props) {
-      swiper.slideToLoop(0, 0, false);
+      swiper.slideToLoop(0, 0, false)
     }
-  }, [props, swiper]);
+  }, [props, swiper])
 
   // const refreshData = () => {
   //   console.log("refreshings");
@@ -97,24 +98,24 @@ export default function HomeMovieCategories(props) {
     <>
       {/* {refreshData()} */}
 
-      <div className="category mt-20 lg:mt-16 md:mt-8">
-        <div className="category__title w-4/5 mx-auto">
-          <div className="category-title-wrapper relative">
+      <div className='category mt-20 lg:mt-16 md:mt-8'>
+        <div className='category__title w-4/5 mx-auto'>
+          <div className='category-title-wrapper relative'>
             <Link
               href={`/[category]`}
               as={`/${
-                movieData.title?.toLowerCase().includes("trending")
-                  ? "Trending"
+                movieData.title?.toLowerCase().includes('trending')
+                  ? 'Trending'
                   : movieData.title
               }`}
             >
               <a>
-                <div className="flex items-center">
-                  <h1 className="category__title-text font-bold font-secondary text-3xl text-gray-700 underline md:text-[1.75rem] sm:text-2xl">
+                <div className='flex items-center'>
+                  <h1 className='category__title-text font-bold font-secondary text-3xl text-gray-700 underline md:text-[1.75rem] sm:text-2xl'>
                     {movieData.title}
                   </h1>
-                  <span className="ml-3 mt-1 xsm:ml-2">
-                    <ArrowNarrowRightIcon className="w-9 h-9 text-green-600 xsm:w-7 xsm:h-7" />
+                  <span className='ml-3 mt-1 xsm:ml-2'>
+                    <ArrowNarrowRightIcon className='w-9 h-9 text-green-600 xsm:w-7 xsm:h-7' />
                   </span>
                 </div>
               </a>
@@ -122,19 +123,19 @@ export default function HomeMovieCategories(props) {
 
             <div
               // className="category__title-bg"
-              className="category__title-bg absolute -left-12 transform -translate-y-2/4 -z-1"
+              className='category__title-bg absolute -left-12 transform -translate-y-2/4 -z-1'
             >
               <Image
-                src="/static/assets/section-title-bg-shape.png"
-                width="143.38"
-                height="130.21"
-                placeholder="blur"
-                alt="category title background shape"
+                src='/static/assets/section-title-bg-shape.png'
+                width='143.38'
+                height='130.21'
+                placeholder='blur'
+                alt='category title background shape'
               />
             </div>
           </div>
         </div>
-        <div className="category__swiper relative mt-14 lg:mt-10 lg:overflow-hidden">
+        <div className='category__swiper relative mt-14 lg:mt-10 lg:overflow-hidden'>
           {movieData.data ? (
             <>
               <Swiper
@@ -187,11 +188,11 @@ export default function HomeMovieCategories(props) {
                 navigation={
                   (true,
                   {
-                    nextEl: ".swiper-navigation-next",
-                    prevEl: ".swiper-navigation-prev",
+                    nextEl: '.swiper-navigation-next',
+                    prevEl: '.swiper-navigation-prev',
                   })
                 }
-                className="category__myswiper mySwiper w-[95%] mx-auto pt-10"
+                className='category__myswiper mySwiper w-[95%] mx-auto pt-10'
               >
                 {/* <div className="test"> */}
                 {movieData.data.map((movie, index) => {
@@ -199,8 +200,8 @@ export default function HomeMovieCategories(props) {
                   return (
                     <SwiperSlide
                       key={index}
-                      style={{ width: "auto" }}
-                      className="group category__swiperslide"
+                      style={{ width: 'auto' }}
+                      className='group category__swiperslide'
                     >
                       <Link
                         href={`/movie/[movieId]`}
@@ -208,22 +209,22 @@ export default function HomeMovieCategories(props) {
                         as={`/movie/${movie.title}-${movie.id}`}
                       >
                         <a>
-                          <div className="category__movie-banner overflow-hidden rounded relative">
+                          <div className='category__movie-banner overflow-hidden rounded relative'>
                             {/* {movie.img.blurDataURL ? ( */}
-                            <div className="category__movie-canvas relative block h-[340px] xl:h-[360px] lg:h-[380px] transform group-hover:scale-110 transition duration-500">
+                            <div className='category__movie-canvas relative block h-[340px] xl:h-[360px] lg:h-[380px] transform group-hover:scale-110 transition duration-500'>
                               <BlurhashCanvas
                                 punch={1}
                                 hash={movie.img.blurDataURL.hash}
                                 width={movie.img.blurDataURL.height}
                                 height={movie.img.blurDataURL.width}
-                                className="absolute left-0 top-0 w-full h-full inset-0"
+                                className='absolute left-0 top-0 w-full h-full inset-0'
                               />
 
                               <Image
                                 src={movie.img.src}
-                                layout="fill"
+                                layout='fill'
                                 // alt={movie.title}
-                                className="object-cover w-full"
+                                className='object-cover w-full'
                               />
                             </div>
                             {/* )  */}
@@ -238,18 +239,18 @@ export default function HomeMovieCategories(props) {
                           )
                           
                           } */}
-                            <figure className="category__effect-zoe">
+                            <figure className='category__effect-zoe'>
                               {/* <img src="https://tympanus.net/Development/HoverEffectIdeas/img/25.jpg" alt="img25"/> */}
                               <figcaption>
                                 {/* <h2>
                             Creative <span>Zoe</span>
                           </h2> */}
 
-                                <p className="category__description text-md">
+                                <p className='category__description text-md'>
                                   {movie.overview.length > 125
                                     ? movie.overview
                                         .substring(125, 0)
-                                        .concat(" ...")
+                                        .concat(' ...')
                                     : movie.overview}
                                 </p>
                               </figcaption>
@@ -257,55 +258,55 @@ export default function HomeMovieCategories(props) {
                           </div>
 
                           <div
-                            className="category__movie-summary my-3"
+                            className='category__movie-summary my-3'
                             // ref={divRef}
                           >
-                            <h1 className="category__movie-title text-gray-700 font-bold text-base group-hover:underline">
+                            <h1 className='category__movie-title text-gray-700 font-bold text-base group-hover:underline'>
                               {movie.title.length > 22
-                                ? movie.title.substring(0, 22).concat(" ...")
+                                ? movie.title.substring(0, 22).concat(' ...')
                                 : movie.title}
                             </h1>
-                            <span className="category__movie-date text-xs text-gray-600">
-                              {movie.release_date?.split("-")[0] ?? ""}
+                            <span className='category__movie-date text-xs text-gray-600'>
+                              {movie.release_date?.split('-')[0] ?? ''}
                             </span>
                           </div>
                         </a>
                       </Link>
                     </SwiperSlide>
-                  );
+                  )
                 })}
                 {/* </div> */}
-                <div className="swiper-navigation-wrapper">
-                  <div className="swiper-navigation-prev swiper-navigation-btn left-5 xl:right-7">
-                    <ChevronLeftIcon className="h-6 w-6 text-white transition duration-300" />
+                <div className='swiper-navigation-wrapper'>
+                  <div className='swiper-navigation-prev swiper-navigation-btn left-5 xl:right-7'>
+                    <ChevronLeftIcon className='h-6 w-6 text-white transition duration-300' />
                   </div>
-                  <div className="swiper-navigation-next absolute swiper-navigation-btn right-5">
-                    <ChevronRightIcon className="h-6 w-6 text-white transition duration-300" />
+                  <div className='swiper-navigation-next absolute swiper-navigation-btn right-5'>
+                    <ChevronRightIcon className='h-6 w-6 text-white transition duration-300' />
                   </div>
                 </div>
-                <div className="swiper-pagination"></div>
+                <div className='swiper-pagination'></div>
               </Swiper>
-              <div className="category__content-bg-shape absolute -top-10 left-0 w-full">
+              <div className='category__content-bg-shape absolute -top-10 left-0 w-full'>
                 <CategoryBgShape
                   className={`mx-auto w-full ${
-                    movieData.reverse ? "-scale-x-1" : "scale-x-1"
+                    movieData.reverse ? '-scale-x-1' : 'scale-x-1'
                   }`}
-                  viewBox="0 0 1440 250"
-                  preserveAspectRatio="none"
+                  viewBox='0 0 1440 250'
+                  preserveAspectRatio='none'
                 />
                 <CategoryBgShape
                   className={`mx-auto ${
-                    movieData.reverse ? "scale-x-1" : "-scale-x-1"
+                    movieData.reverse ? 'scale-x-1' : '-scale-x-1'
                   } rotate-180`}
-                  viewBox="0 0 1440 250"
-                  preserveAspectRatio="none"
-                  width="100%"
+                  viewBox='0 0 1440 250'
+                  preserveAspectRatio='none'
+                  width='100%'
                 />
               </div>
             </>
           ) : (
-            <div className="category__notfound h-24 md:h-28 md:mb-10 text-lg sm:text-base flex items-center justify-center font-medium text-gray-600 md:w-4/5 md:text-center md:flex-col md:mx-auto">
-              <Notfound className="w-16 h-16 mr-4 md:mb-2 md:mr-0 xsm:w-12 xsm:h-12" />{" "}
+            <div className='category__notfound h-24 md:h-28 md:mb-10 text-lg sm:text-base flex items-center justify-center font-medium text-gray-600 md:w-4/5 md:text-center md:flex-col md:mx-auto'>
+              <Notfound className='w-16 h-16 mr-4 md:mb-2 md:mr-0 xsm:w-12 xsm:h-12' />{' '}
               We couldn't find any movies for you, Please try again later
             </div>
           )}
@@ -334,5 +335,5 @@ export default function HomeMovieCategories(props) {
         </div>
       </div>
     </>
-  );
+  )
 }
