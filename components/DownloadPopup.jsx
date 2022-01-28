8ddcee9d4246eca5964620a731aa0220ad3e7a45
Kaung Sithu <kgcthu.kev@gmail.com>
2022-01-28 07:21:16 +0630

    center download btn

--- components/DownloadPopup.jsx
+++ components/DownloadPopup.jsx
@@ -12,15 +12,17 @@ export default function DownloadPopup({ isOpen, result }) {
     <>
       {/* <div className="download-overlay w-full h-full fixed bg-gray-800 top-0 left-0 opacity-50 z-100"></div> */}
       <div className='download fixed w-80 bg-gray-100 p-10 h-40 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-        <div className='download__container flex flex-row align-center justify-center'>
-          {result.map((detail, index) => (
-            <button
-              key={index}
-              className='py-3 px-6 bg-green-400 rounded text-white hover:bg-green-500 transition flex items-center text-base mx-2'
-            >
-              <a href={detail.url}>{detail.quality}</a>
-            </button>
-          ))}
+        <div className='download__container mx-3'>
+          <div className='flex flex-row translate-y-1/3 justify-center align-center space-x-3'>
+            {result.map((dInfo, index) => (
+              <button
+                key={index}
+                className='py-3 px-6 bg-green-400 rounded text-white hover:bg-green-500 transition flex items-center text-base'
+              >
+                <a href={dInfo.url}>{dInfo.quality}</a>
+              </button>
+            ))}
+          </div>
         </div>
       </div>
     </>
