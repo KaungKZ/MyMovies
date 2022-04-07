3efaf4bb7b1dcc04a000f9c6771fc93da09f12df
Kaung Khant Zaw <kkz@sinwattana.com>
2022-04-07 14:53:46 +0630

    added pagination in category page

--- components/AllMoviesByCategory.jsx
+++ components/AllMoviesByCategory.jsx
@@ -46,11 +46,14 @@ export default function AllMoviesByCategory(props) {
     // console.log(test);
   }, []);
 
-  useEffect(() => {}, []);
+  // useEffect(() => {}, []);
 
   // console.log(totalPagination);
 
   function generateTotalPagination(total_pages) {
+    console.log("calculating total");
+    // console.log(total_pages, start);
+    // console.log("rendered total pagination function");
     let arr = [];
     for (let i = 1; i <= total_pages; i++) {
       arr.push(i);
@@ -59,55 +62,45 @@ export default function AllMoviesByCategory(props) {
     return arr;
   }
 
-  async function handleGoSpecificPage(e) {
-    // const response = await axios.get(
-    //   results.url + `&page=${e.currentTarget.dataset.page}`
-    // ).then(data => {
+  async function getMoviesByPage(e) {
+    const pageNumber = e.currentTarget.dataset.page;
+    const response = await axios.get(results.url + `&page=${pageNumber}`).then(
+      async (data) =>
+        Promise.all(
+          data.data.results.map(async (one) => {
+            return await axios
+              .post("/api/getBlurhash", {
+                url: `https://image.tmdb.org/t/p/w500${one.poster_path}`,
+              })
+              .then(({ data }) => {
+                return {
+                  ...one,
+                  img: { ...data.img, blurDataURL: data.blurhash },
+                };
+              })
+              .catch(() => ({ ...one, img: { blurDataURL: null } }));
+          })
+        ).then((values) => ({
+          success: true,
+          data: values,
+          page: data.data.page,
+        })),
 
-    // })
-
-    // const getBlurHash = await axios.post("/api/getBase64", {
-    //   url: `https://image.tmdb.org/t/p/w500${one.poster_path}`,
-    // });
-
-    // console.log(getBlurHash);
-
-    console.log("going to" + e.currentTarget.dataset.page);
-
-    const response = await axios
-      .get(results.url + `&page=${e.currentTarget.dataset.page}`)
-      .then(
-        async (data) =>
-          Promise.all(
-            data.data.results.map(async (one) => {
-              return await axios
-                .post("/api/getBlurhash", {
-                  url: `https://image.tmdb.org/t/p/w500${one.poster_path}`,
-                })
-                .then(({ data }) => {
-                  return {
-                    ...one,
-                    img: { ...data.img, blurDataURL: data.blurhash },
-                  };
-                })
-                .catch(() => ({ ...one, img: { blurDataURL: null } }));
-            })
-          ).then((values) => ({
-            success: true,
-            data: values,
-            page: data.data.page,
-          })),
-
-        () => ({ success: false })
-      );
+      () => ({ success: false })
+    );
 
     // console.log(response);
     console.log("done ! ");
 
     if (response.success) {
-      generatePagination(response);
+      generatePagination(response, pageNumber);
     }
+  }
 
+  async function handleGoSpecificPage(e) {
+    setLoading(true);
+
+    getMoviesByPage(e);
     // console.log(response);
 
     // generatePagination(response.data.)
@@ -115,15 +108,82 @@ export default function AllMoviesByCategory(props) {
     // console.log(e.target.innerHTML);
   }
 
-  function generatePagination(data) {
-    // console.log(data);
+  function generatePagination(data, pageNumber) {
+    // clone the original array
+    // console.log(router.query);
+    // const _total_pages = [...results.total_pages]
+    // remove numbers infront of selected number except the closest one before
+
     setResults({
       ...results,
       data: data.data,
       page: data.page,
     });
+
+    // router.push({
+    //   pathname: router.pathname,
+    //   query: {
+    //     category: router.query.category,
+    //     page: data.page,
+    //   },
+    // });
+
+    // router.push(`${router.asPath}/?page=${data.page}`, undefined, {
+    //   shallow: true,
+    // });
+
+    // console.log(router.query.category[0]);
+
+    // router.push(`?page=${data.page}`, undefined, {
+    //   shallow: true,
+    // });
+
+    // console.log(data.page);
+
+    const category = router.query.category;
+    const page = data.page;
+
+    // router.push(
+    //   {
+    //     pathname: `/[category]`,
+    //     query: { ...router.query, page: page },
+    //   },
+    //   `/${category}?page=${page}`,
+    //   { shallow: true }
+    // );
+
+    // router.push(`${router.asPath}/${data.page}`);
+
+    setLoading(false);
+
     // console.log(results);
   }
+
+  // useEffect(() => {
+  //   // Always do navigations after the first render
+  //   router.push("/?counter=10", undefined, { shallow: true });
+  // }, []);
+
+  // useEffect(() => {
+  //   const category = router.query.category;
+  //   const page = data.page;
+
+  //   if (loading === false) {
+  //     router.push(
+  //       {
+  //         pathname: `/[category]`,
+  //         query: { category: category, page: page },
+  //       },
+  //       `/${category}?page=${page}`,
+  //       { shallow: true }
+  //     );
+  //   }
+
+  // }, [loading]);
+
+  // console.log(results.page, totalPagination);
+
+  // console.log(totalPagination);
 
   // console.log(results);
 
@@ -157,24 +217,54 @@ export default function AllMoviesByCategory(props) {
 
         {results && results.data ? (
           <div className="movies__wrapper mt-16">
-            <div className="movies__list grid grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 xsm:grid-cols-1 gap-x-7">
+            <div className="movies__list grid grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 xsm:grid-cols-1 gap-x-7 gap-y-5">
               {results.data.map((movie) => {
                 return <MovieList movie={movie} key={movie.id} />;
               })}
             </div>
-            <div className="movies__pagination">
+            <div className="movies__pagination flex justify-center mt-12">
               {/* {console.log(totalPagination)} */}
-              <button className="left-pagination">
+              <button className="left-pagination mr-4">
                 <ChevronLeftIcon className="h-6 w-6 text-black transition duration-300" />
               </button>
 
-              <div className="page-numbers">
-                {totalPagination.slice(0, 5).map((v) => {
+              <div className="page-numbers ">
+                <button
+                  className={`page-number ${
+                    totalPagination[0] === results.page ? "active" : ""
+                  } py-1 px-2 mr-2`}
+                  key={totalPagination[0]}
+                  data-page={totalPagination[0]}
+                  onClick={handleGoSpecificPage}
+                >
+                  {totalPagination[0]}
+                </button>
+                {results.page >= 6 ? <span>...</span> : <></>}
+                {[
+                  // if total page is over 6
+                  ...(totalPagination.length > 6
+                    ? // if current page is less than 6
+                      results.page < 6
+                      ? totalPagination.slice(1, 6)
+                      : // else if current page is within 4 pages away from last page
+                      results.page >= results.total_pages - 4
+                      ? totalPagination.slice(
+                          totalPagination[totalPagination.length - 7], // show only numbers which are 7 index away from last
+                          totalPagination[totalPagination.length - 2] // remove last and show before last
+                        )
+                      : // anything else (if current page is not less than 6 or within 4 pages away from last page)
+                        totalPagination.slice(
+                          results.page - 3,
+                          results.page + 2
+                        )
+                    : // if total page is below 6
+                      totalPagination),
+                ].map((v) => {
                   return (
                     <button
                       className={`page-number ${
                         v === results.page ? "active" : ""
-                      }`}
+                      } py-1 px-2 mr-2`}
                       key={v}
                       data-page={v}
                       onClick={handleGoSpecificPage}
@@ -183,13 +273,17 @@ export default function AllMoviesByCategory(props) {
                     </button>
                   );
                 })}
-                <span>...</span>
+                {results.page < results.total_pages - 4 ? (
+                  <span>...</span>
+                ) : (
+                  <></>
+                )}
                 <button
                   className={`page-number ${
                     totalPagination[totalPagination.length - 1] === results.page
                       ? "active"
                       : ""
-                  }`}
+                  } py-1 px-2`}
                   key={totalPagination[totalPagination.length - 1]}
                   data-page={totalPagination[totalPagination.length - 1]}
                   onClick={handleGoSpecificPage}
@@ -197,7 +291,7 @@ export default function AllMoviesByCategory(props) {
                   {totalPagination[totalPagination.length - 1]}
                 </button>
               </div>
-              <button className="right-pagination">
+              <button className="right-pagination ml-4">
                 <ChevronRightIcon className="h-6 w-6 text-black transition duration-300" />
               </button>
             </div>
